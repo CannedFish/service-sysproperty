9558d9158b9e29a73fdf5a20d1fdcb1cde6e308b
LiangGuanyu <lianggy0719@126.com>
2015-08-18 17:15:59 +0800

    WEB-128 systemproperty works well

--- test/client.js
+++ test/client.js
@@ -1,19 +1,21 @@
-var requireProxy = require('../../../app/demo-rio/sdk/lib/requireProxy').requireProxy;
-requireProxy(['sysproperty'], function(sysproperty) {
-  /* sysproperty.get("locale", function(ret) {
-    if (ret.err) return console.log('get error:', ret.err);
-    console.log('locale:', ret.ret);
-  });*/
-  var obj = {
-    "key": "locale",
-    "value": "en"
-  };
-  sysproperty.set(obj, function(ret) {
-    if (ret.err) return console.log(ret.err);
-    console.log('set property success!');
-  });
+var requireProxy = require('../../../app/demo-rio/sdk/lib/requireProxy').requireProxySync;
+var sysproperty = requireProxy('sysproperty');
 
-  sysproperty.on('change', function(ret) {
-    console.log('change Event :', ret);
-  });
-}) 
+sysproperty.get("locale", function(ret) {
+  if (ret.err) return console.log('get error:', ret.err);
+  console.log('locale:', ret.ret);
+});
+
+/* var obj = { */
+  // "key": "locale",
+  // "value": "en"
+// };
+// sysproperty.set(obj, function(ret) {
+  // if (ret.err) return console.log(ret.err);
+  // console.log('set property success!');
+/* }); */
+
+sysproperty.on('change', function(ret) {
+  console.log('change Event :', ret);
+});
+
